9a7640472b5ff906ae32961dc1c42b499d26e305
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-11-30 13:55:21 +0000

    [Finish]guest rogin

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "guestuserでログインしました。"
+  end
+
   protected
 
   def after_sign_in_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :ensure_guest_user, only: [:edit]
   def index
     @users = User.all
     @book = Book.new
@@ -46,4 +46,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
 end
